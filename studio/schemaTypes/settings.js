1cc87af1a85350b046b14dd363754774ca405252
Ruby <rubyseresin@gmail.com>
2025-02-12 14:20:36 +0000

    added logos to settings schema

--- studio/schemaTypes/settings.js
+++ studio/schemaTypes/settings.js
@@ -10,8 +10,48 @@ export default {
       validation: (Rule) => Rule.required(),
     },
     {
-      name: 'logo',
-      title: 'Logo',
+      name: 'logo1',
+      title: 'Logo 1',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logo2',
+      title: 'Logo 2',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logo3',
+      title: 'Logo 3',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logo4',
+      title: 'Logo 4',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logo5',
+      title: 'Logo 5',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'logo6',
+      title: 'Logo 6',
       type: 'image',
       options: {
         hotspot: true,
