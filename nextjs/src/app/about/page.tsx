7f5663c93bfa47e6c11348d39584847766239884
codehath <codingfarhath@gmail.com>
2025-02-15 18:43:49 +0000

    fix: Improved about page. Made padding above logo not apply on mobile, removed empty padding div at bottom of page and added space between paragraphs using 'prose' from newly installed tailwindcss/typography package.

--- nextjs/src/app/about/page.tsx
+++ nextjs/src/app/about/page.tsx
@@ -5,34 +5,25 @@ import { PortableText } from "@portabletext/react";
 
 const SETTINGS_QUERY = `*[_type == "settings"][0]`;
 
-const renderSocialLink = (type, url) => (
-  <SocialLink type={type} url={url} color="black" />
-);
+const renderSocialLink = (type, url) => <SocialLink type={type} url={url} color="black" />;
 
 export default async function AboutPage() {
   const settings = await client.fetch(SETTINGS_QUERY);
 
   return (
-    <div className="container mx-auto max-w-7xl py-20">
+    <div className="container mx-auto max-w-7xl md:py-20">
       <div className="grid gap-20 lg:grid-cols-4">
         {/* Left Column */}
         <div className="space-y-16 lg:col-span-3">
-          <Image
-            src="/logos/manifest-text-logo-black.svg"
-            alt="Logo"
-            width={250}
-            height={48}
-          />
-          <div className="text-xl font-normal text-black">
+          <Image src="/logos/manifest-text-logo-black.svg" alt="Logo" width={250} height={48} />
+          <div className="prose text-xl font-normal text-black">
             <PortableText value={settings.about} />
           </div>
           <div className="flex items-center gap-4">
-            {settings.instagram &&
-              renderSocialLink("instagram", settings.instagram)}
+            {settings.instagram && renderSocialLink("instagram", settings.instagram)}
             {settings.email && renderSocialLink("email", settings.email)}
           </div>
         </div>
-        <div className="h-96 lg:h-auto"></div>
       </div>
     </div>
   );
