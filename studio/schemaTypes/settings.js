39e3f837ae91540ab8e5f4b9a0ce2e580973b1fe
codehath <codingfarhath@gmail.com>
2025-02-15 20:56:42 +0000

    refactor: removed old commented code

--- studio/schemaTypes/settings.js
+++ studio/schemaTypes/settings.js
@@ -1,56 +1,25 @@
-// export default {
-//   name: 'settings',
-//   title: 'Settings',
-//   type: 'document',
-//   fields: [
-//     {
-//       name: 'name',
-//       title: 'Company Name',
-//       type: 'string',
-//       validation: (Rule) => Rule.required(),
-//     },
-//     {
-//       name: 'about',
-//       title: 'About',
-//       type: 'array',
-//       of: [{type: 'block'}],
-//       validation: (Rule) => Rule.required(),
-//     },
-//     {
-//       name: 'instagram',
-//       title: 'Instagram',
-//       type: 'url',
-//     },
-//     {
-//       name: 'email',
-//       title: 'Email',
-//       type: 'string',
-//       validation: (Rule) => Rule.required().email(),
-//     },
-//   ],
-// }
-
 const createLogoField = (number) => ({
   name: `logo${number}`,
   title: `Logo ${number}`,
   type: 'image',
-  description: number === 1 
-    ? 'Primary logo - Used in header - black version'
-    : number === 2
-    ? 'Primary logo - white version'
-    : number === 3
-    ? 'Full Logo (small) - Compact version optimized for mobile devices and small spaces'
-    : number === 4
-    ? 'Full logo (large) - Used as button on landing page'
-    : number === 5
-    ? 'Wordmark logo balck - Used on about page'
-    : number === 6
-    ? 'Wordmark logo white - Used in footer'
-    : 'Additional logo - For special campaigns or alternative branding needs',
+  description:
+    number === 1
+      ? 'Primary logo - Used in header - black version'
+      : number === 2
+        ? 'Primary logo - white version'
+        : number === 3
+          ? 'Full Logo (small) - Compact version optimized for mobile devices and small spaces'
+          : number === 4
+            ? 'Full logo (large) - Used as button on landing page'
+            : number === 5
+              ? 'Wordmark logo balck - Used on about page'
+              : number === 6
+                ? 'Wordmark logo white - Used in footer'
+                : 'Additional logo - For special campaigns or alternative branding needs',
   options: {
     hotspot: true,
   },
-});
+})
 
 export default {
   name: 'settings',
