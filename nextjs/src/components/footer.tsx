0937a079b17585f3b907bbbe172bd8566adc853a
Ruby <rubyseresin@gmail.com>
2025-02-18 15:22:04 +0000

    fix: footer spacing and alignment

--- nextjs/src/components/footer.tsx
+++ nextjs/src/components/footer.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useSettings } from "@/contexts/SettingsContext";
@@ -6,19 +6,26 @@ import { urlFor } from "@/sanity/image";
 
 export function Footer() {
   const settings = useSettings();
-  const logoUrl = settings.logo6 ? urlFor(settings.logo6).url() : "/logos/manifest-text-logo-white.svg";
+  const logoUrl = settings.logo6
+    ? urlFor(settings.logo6).url()
+    : "/logos/manifest-text-logo-white.svg";
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="bg-black px-4 py-12 text-white md:px-6 lg:px-8">
       <div className="container mx-auto max-w-8xl">
         <div className="flex flex-col items-start justify-between space-y-4 md:flex-row md:items-center md:space-y-0">
-          <div className="text-sm">
+          <div className="text-sm w-1/3 items-start">
             <Image src={logoUrl} alt="Manifest Music" width={160} height={40} />
-            <div className="mt-2 text-xs text-gray-300"> {currentYear} {settings.name} All Rights Reserved.</div>
+            <div className="mt-2 text-[10px] leading-none text-gray-300">
+              {" "}
+              {currentYear} {settings.name} All Rights Reserved.
+            </div>
           </div>
-          <div className="text-sm">Music Publishing • Management • Creative Services</div>
-          <div className="text-sm">
+          <div className="flex flex-col text-sm w-1/3 items-center">
+            Music Publishing • Management • Creative Services
+          </div>
+          <div className="flex flex-col justify-between text-sm w-1/3 lg:items-end md:items-end sm:items-start">
             <a href={`mailto:${settings.email}`} className="hover:underline">
               {settings.email}
             </a>
