33dde4d26e851adac81c5bd3682ed7a9de138e59
Ruby <rubyseresin@gmail.com>
2025-02-19 12:15:33 +0000

    fix: updated sanity image type in sanity image file

--- nextjs/src/sanity/image.ts
+++ nextjs/src/sanity/image.ts
@@ -1,6 +1,16 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./client";
 
+interface SanityImageSource {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+  alt?: string;
+  caption?: string;
+}
+
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source: SanityImageSource) => builder.image(source);
