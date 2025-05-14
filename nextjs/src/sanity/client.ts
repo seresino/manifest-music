fc1984c2c64e0498ff2c9a623afdcbd8c7ad7146
Ruby <rubyseresin@gmail.com>
2025-05-14 19:48:32 +0100

    fix: changed client to fetch from sanity cdn

--- nextjs/src/sanity/client.ts
+++ nextjs/src/sanity/client.ts
@@ -4,6 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2025-02-15",
-  useCdn: false, // Disable CDN since dataset is private
-  token: process.env.SANITY_API_READ_TOKEN,
+  useCdn: false,
 });
