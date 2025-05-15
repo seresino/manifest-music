c8c9e11d3f77d132e04f4e778bca3425c5044460
Ruby <rubyseresin@gmail.com>
2025-05-15 20:51:29 +0100

    fix: added more padding to bottom of artist grid

--- nextjs/src/app/roster/page.tsx
+++ nextjs/src/app/roster/page.tsx
@@ -13,7 +13,7 @@ export default async function IndexPage() {
   );
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto pb-8">
       <ArtistGrid artists={artists} />
     </div>
   );
