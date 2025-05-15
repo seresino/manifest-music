19755c13244634b3933eacd7a0f0a48d19e292ad
Ruby <rubyseresin@gmail.com>
2025-05-15 19:46:55 +0100

    fix: changed grid styling to 4 columns large

--- nextjs/src/components/artist-grid.tsx
+++ nextjs/src/components/artist-grid.tsx
@@ -11,7 +11,7 @@ export function ArtistGrid({ artists }: ArtistGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {artists.map((artist) => (
         <ArtistCard key={artist._id} artist={artist} />
       ))}
