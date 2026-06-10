ef2fb3f6923b76d0eec005818b5cf9085706a3df
Ruby <rubyseresin@gmail.com>
2026-06-10 15:13:27 +0100

    Add revalidation to settings fetch for about page updates

--- nextjs/src/components/SettingsProvider.tsx
+++ nextjs/src/components/SettingsProvider.tsx
@@ -3,10 +3,20 @@ import { QUERIES } from "@/constants/queries";
 import type { Settings } from "@/contexts/SettingsContext";
 import SettingsProviderClient from "./SettingsProviderClient";
 
-async function SettingsProvider({ children }: { children: React.ReactNode }) {
-  const initialSettings = await client.fetch<Settings>(QUERIES.SETTINGS);
+const options = { next: { revalidate: 30 } };
 
-  return <SettingsProviderClient initialSettings={initialSettings}>{children}</SettingsProviderClient>;
+async function SettingsProvider({ children }: { children: React.ReactNode }) {
+  const initialSettings = await client.fetch<Settings>(
+    QUERIES.SETTINGS,
+    {},
+    options,
+  );
+
+  return (
+    <SettingsProviderClient initialSettings={initialSettings}>
+      {children}
+    </SettingsProviderClient>
+  );
 }
 
 export default SettingsProvider;
